975e5aeb2fc5ba33948e1f7264fd0d4f00d0012f
fari-panisher <farhad.13ahmadi.83@gmail.com>
2023-10-11 14:34:06 +0330

    refactor(displayYear & defaultYear & year s &  maxYear & displayError & insuranceCase & calculatePrice & diffrence) : change Procedural programming to functional programming
    Des :
    + separate all functions and make many small functions for use it easier.
    + function for now year(max year)
    + function for  default year
    + function for  sending to DOM
    + validation function for checking form
    + error function, when user doesn't fill the form
    + function for  value of the form
    + function for  price of insurance
    
    - separate all works in project, and make special function for each work :)

--- founctional programming/js/app.js
+++ founctional programming/js/app.js
@@ -9,11 +9,71 @@ form.addEventListener("submit", checkSubmit);
 
 
 // functions
+
 // for loading the page 
 function afterLoad() {
     displayYears()
 }
 
+function displayYears() {
+    let minYear = maxYear() - 20
+
+    const selectYear = document.querySelector('#year')
+
+    // defult year
+    defaultYear(selectYear)
+    // 20 years ago
+    years(selectYear, minYear)
+}
+// defult year
+function defaultYear(selectYear) {
+    // creat optin for select
+    let creartOption = document.createElement('option')
+    // pass empty value to option
+    creartOption.value = ""
+    // pass defult text to option
+    creartOption.innerText = `- انتخاب کن-`
+    // send option to select
+    selectYear.appendChild(creartOption)
+}
+
+// years for pass to DOM
+function years(selectYear, min) {
+    for (let i = maxYear(); i >= min; i--) {
+        // create option element
+        let creartOption = document.createElement('option')
+        // pass value to option
+        creartOption.value = i
+        // pass text to option element
+        creartOption.innerText = `سال ${i}`
+        // send option to select element
+        selectYear.appendChild(creartOption)
+    }
+}
+
+// find now year
+function maxYear() {
+    // new date
+    let now = new Date().toLocaleDateString('fa-IR')
+    // get full persion year
+    now.slice(0, 4)
+    // change persion string to english number
+    let
+        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+        fixNumbers = function (str) {
+            if (typeof str === 'string') {
+                for (var i = 0; i < 10; i++) {
+                    str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+                }
+            }
+            return str;
+        };
+    // max year
+    let maxYear = parseInt(fixNumbers(now))
+    return maxYear
+
+}
 // validation for checking form
 function checkSubmit(e) {
     // not load when submit form
@@ -24,88 +84,20 @@ function checkSubmit(e) {
     const level = document.querySelector("input[name='level']:checked").value
 
     // check form validity
+    validaition(make, year, level)
+}
+
+// checking form
+function validaition(make, year, level) {
     if (make === "" || year === "" || level === "") {
-        console.log("error :(");
         displayError("لطفا فرم را کامل پر کنید")
     } else {
-        // console.log(insuranceCase(make, year, level.value));
-
-        let insuranse = {
-            make: make,
-            year: year,
-            level: level
-        }
-        calculatePrice(insuranse)
+        // function for form value
+        insuranceCase(make, year, level)
     }
 }
 
-// value of form
-// function insuranceCase(inCarMake, inYear, inLevel) {
-//     return {
-//         carMake: inCarMake,
-//         Yera: inYear,
-//         level: inLevel
-//     }
-// }
-
-
-function calculatePrice(info) {
-    let price = 0,
-        base = 2000000
-    // + Calculate Make 
-    /* 
-    make:1      =>      1.15
-    make:2      =>      1.30
-    make:3      =>      1.80
-    */
-
-    switch (info.make) {
-        case "1":
-            price = base * 1.15
-            break;
-
-        case "2":
-            price = base * 1.30
-            break;
-        case "3":
-            price = base * 1.80
-            break;
-    }
-
-    // + Calculate Year
-    // get the year
-
-    // change persion string to english number
-    let year = info.year
-    diffrence = function (year) {
-        let
-            persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
-            arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
-            fixNumbers = function (str) {
-                if (typeof str === 'string') {
-                    for (var i = 0; i < 10; i++) {
-                        str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
-                    }
-                }
-                return str;
-            };
-        let now = new Date().toLocaleDateString("fa-IR");
-        let nowYear = now.slice(0, 4)
-        let max = parseInt(fixNumbers(nowYear))
-
-
-        year = max - year
-        return year
-    }
-
-    // price
-    price = price - ((diffrence(year) * 3) / 100) * price
-    console.log(price);
-
-
-}
 // error function
-
 function displayError(msg) {
     // creat div
     const errorDiv = document.createElement("div")
@@ -122,55 +114,53 @@ function displayError(msg) {
     }, 5000);
 }
 
-
-
-function displayYears() {
-
-    // new date
-    let now = new Date().toLocaleDateString('fa-IR')
-    // get full persion year
-    now.slice(0, 4)
-
-    // change persion string to english number
-
-    let
-        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
-        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
-        fixNumbers = function (str) {
-            if (typeof str === 'string') {
-                for (var i = 0; i < 10; i++) {
-                    str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
-                }
-            }
-            return str;
-        };
-
-    // max year
-    let maxYear = parseInt(fixNumbers(now))
-    // min year
-    let minYear = maxYear - 20
-
-    const selectYear = document.querySelector('#year')
-
-    // creat optin for select
-    let creartOption = document.createElement('option')
-    // pass empty value to option
-    creartOption.value = ""
-    // pass defult text to option
-    creartOption.innerText = `- انتخاب کن-`
-    // send option to select
-    selectYear.appendChild(creartOption)
-    // create loop for find years
-    for (let i = maxYear; i >= minYear; i--) {
-        // create option element
-        let creartOption = document.createElement('option')
-        // pass value to option
-        creartOption.value = i
-        // pass text to option element
-        creartOption.innerText = `سال ${i}`
-        // send option to select element
-        selectYear.appendChild(creartOption)
+// value of form
+function insuranceCase(inCarMake, inYear, inLevel) {
+    let info = {
+        carMake: inCarMake,
+        carYear: inYear,
+        level: inLevel
     }
+    calculatePrice(info)
+}
 
+// price of insurance
+function calculatePrice(info) {
+    let price = 0,
+        base = 2000000
 
+    // price
+    price = CalculateCar(price, base, info) - ((diffrence(info) * 3) / 100) * CalculateCar(price, base, info)
+    console.log(price);
+}
+
+// price of car
+function CalculateCar(price, base, info) {
+    // + Calculate Make 
+    /* 
+    make:1      =>      1.15
+    make:2      =>      1.30
+    make:3      =>      1.80
+    */
+
+    switch (info.carMake) {
+        case "1":
+            price = base * 1.15
+            break;
+
+        case "2":
+            price = base * 1.30
+            break;
+        case "3":
+            price = base * 1.80
+            break;
+    }
+    return price
+}
+
+// price of year
+function diffrence(info) {
+    let year = info.carYear
+    year = maxYear() - year
+    return year
 }
