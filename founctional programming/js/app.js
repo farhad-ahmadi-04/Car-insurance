c857d93c450e8406e4cced37aab7adf2f645aa90
fari-panisher <farhad.13ahmadi.83@gmail.com>
2023-10-16 01:35:21 +0330

    Refactor(config) : object of information
    Des :
    + add object and put all datas in this object
    + remove all numbers in project, instead of that use config object.
    + add comment for functions (comment for each paramet)

--- founctional programming/js/app.js
+++ founctional programming/js/app.js
@@ -1,87 +1,107 @@
-// varibales
+// varibales...........
 const form = document.querySelector("#request-quote");
 
 
 
-// event
+// event.................
 document.addEventListener("DOMContentLoaded", afterLoad);
 form.addEventListener("submit", checkSubmit);
 
+// information storage.............. 
+const config = {
+    // prices of insurance
+    prices: {
+        price: 0, // default price of insurance
+        basePrice: 2000000, // base of insurance price,
+    },
+    // The coefficient of cars
+    carsPrace: {
+        make1: 1.15, // praid coefficient
+        make2: 1.3, // optima coefficient
+        make3: 1.8, // porsh coefficient
+    },
+    // type of insurance value
+    typeOfInsurance: {
+        basic: 1.3, // 30%
+        complete: 1.5, // 50%
+    },
+    yearsNumber: 20, // for min year
+};
 
-// functions
+
+// functions.....................
 
 // for loading the page 
 function afterLoad() {
     displayYears()
 }
 
+// make year
 function displayYears() {
-    let minYear = currentYear() - 20
-
+    let minYear = currentYear() - config.yearsNumber
     const selectYear = document.querySelector('#year')
-
     // defult year
-    defaultYear(selectYear)
+    years(selectYear, "", "- انتخاب سال -")
     // 20 years ago
-    years(selectYear, minYear)
-}
-// defult year
-function defaultYear(selectYear) {
-    // creat optin for select
-    let creartOption = document.createElement('option')
-    // pass empty value to option
-    creartOption.value = ""
-    // pass defult text to option
-    creartOption.innerText = `- انتخاب کن-`
-    // send option to select
-    selectYear.appendChild(creartOption)
-}
-
-// years for pass to DOM
-function years(selectYear, min) {
-    for (let i = currentYear(); i >= min; i--) {
-        // create option element
-        let creartOption = document.createElement('option')
-        // pass value to option
-        creartOption.value = i
-        // pass text to option element
-        creartOption.innerText = `سال ${i}`
-        // send option to select element
-        selectYear.appendChild(creartOption)
+    for (let i = currentYear(); i >= minYear; i--) {
+        years(selectYear, i, `سال ${i}`)
     }
 }
 
-// find now year
+// years for pass to DOM
+// + parametr1 : posision for year in DOM
+// + parametr2 : value of year
+// + parametr3 : text of year in select element
+// > output : showing current year until 20 years ago in DOM
+function years(selectYear, i, yearText) {
+    // create option element
+    let creartOption = document.createElement('option')
+    // pass value to option
+    creartOption.value = i
+    // pass text to option element
+    creartOption.innerText = yearText
+    // send option to select element
+    selectYear.appendChild(creartOption)
+}
+
+// find current persian year
+// return : current year
 function currentYear() {
     // new date
     let now = new Date().toLocaleDateString('fa-IR')
     // get full persion year
-    now.slice(0, 4)
-
+    now.slice(0, 4) // remove month and day
     // max year
-    let maxYear = parseInt(englishNumber(now))
+    let maxYear = parseInt(englishNumber(now)) // change to number type with parseInt
     return maxYear
 
 }
 
-// change persion string to english number
-//  get !english max year number
-// give english number
+// change persian string to english string
+//  parametr: !english current year string > get number with type of string
+// return : english number (type of string)
 function englishNumber(str) {
     let
+        // persian number (perasian key board)
         persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+        // arabic number (arabic key board)
         arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g]
+    // type of str should be string
     if (typeof str === 'string') {
+        // start loop from 0 to 9 ( each key board have 10 key numbers )
         for (var i = 0; i < 10; i++) {
+            // replace persian number to english number (type of string)
+            // replace arabic number to english number (type of string)
             str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
         }
     }
     return str;
 }
 
-// validation for checking form
+// checking form
+// output : validaition
 function checkSubmit(e) {
-    // not load when submit form
+    // not refresh page when submit the form
     e.preventDefault();
     // select values
     const make = document.querySelector("#make").value
@@ -89,20 +109,40 @@ function checkSubmit(e) {
     const level = document.querySelector("input[name='level']:checked").value
 
     // check form validity
-    validaition(make, year, level)
+    afterCheckForm(make, year, level)
 }
 
-// checking form
+// validation form
+// + parameter1 : value car input
+// + parameter2 : value year input
+// + parameter3 : value type of insurance
+// returns : value of status
 function validaition(make, year, level) {
+    let status = false
     if (make === "" || year === "" || level === "") {
-        displayError("لطفا فرم را کامل پر کنید")
+        status = false
     } else {
-        // function for form value
+        status = true
+    }
+    return status
+}
+
+// if validate is true call insuranceCase and if validate is false call displayError function
+// parameter1 : the value of car
+// parameter2 : the value of year
+// parameter3 : the value of type of insurance
+// parameter4 : the error message
+function afterCheckForm(make, year, level, error = "لصفا فرم را پر کنید") {
+    if (validaition(make, year, level)) {
         insuranceCase(make, year, level)
+    } else {
+        displayError(error)
     }
 }
 
 // error function
+// + parametr : massage for error
+// > output : showing error in DOM for 5seconds
 function displayError(msg) {
     // creat div
     const errorDiv = document.createElement("div")
@@ -119,35 +159,41 @@ function displayError(msg) {
     }, 5000);
 }
 
-// value of form
+// value of form inputs
+// + parameter1 : value of car input
+// + parameter2 : value of year input
+// + parameter3 : value of type of insutance
 function insuranceCase(inCarMake, inYear, inLevel) {
     let info = {
         carMake: inCarMake,
         carYear: inYear,
         level: inLevel
     }
+    // pass object to calculatePrice function
     calculatePrice(info)
 }
 
 // price of insurance
+// parameter1 : object of value from input  
+// return : price of insurance
 function calculatePrice(info) {
-    let price = 0,
-        base = 2000000
-
-    // price
+    let price = config.prices.price,
+        base = config.prices.basePrice
+    // formula for insurance
     price = (CalculateCar(price, base, info) - ((yearDiscount(info) * 3) / 100) * CalculateCar(price, base, info)) * CalculateLevl(info)
     console.log(price);
 }
 
-// price of car
+// Insurance price based on car factor
+// parameter1 : price of insurance
+// parameter2 : base price of insurance
+// parameter3 : object of value from input 
+// return : price 
 function CalculateCar(price, base, info) {
-
-
     switch (info.carMake) {
         case "1":
             price = base * calculateOfCar(info)
             break;
-
         case "2":
             price = base * calculateOfCar(info)
             break;
@@ -159,42 +205,38 @@ function CalculateCar(price, base, info) {
 }
 
 // Discount of year
-// get object from insuranceCase()
-// give year of discount
+// parameter : object of value from input 
+// return = The year chosen by the user - current year
 function yearDiscount(info) {
     let year = info.carYear
     year = currentYear() - year
     return year
 }
 
+// The coefficient of cars According to car input value
+// parameter : object of value from input 
+// output : The coefficient of cars
 function calculateOfCar(info) {
-    // + Calculate Make 
-    /* 
-    make:1      =>      1.15
-    make:2      =>      1.30
-    make:3      =>      1.80
-    */
     let carValue = info.carMake
     switch (carValue) {
         case "1":
-            return 1.15
+            return config.carsPrace.make1
         case "2":
-            return 1.30
+            return config.carsPrace.make2
         case "3":
-            return 1.80
+            return config.carsPrace.make3
     }
 }
 
+// It takes the type of insurance chosen by the user and gives a numerical insurance type based on the value
+// parameter : object of value from input 
+// return : number type of insurance
 function CalculateLevl(info) {
-    /*
-           basic   =>  increase 30%
-           complete=>  increase 50%
-       */
     let level = info.level
     if (level == 'basic') {
         // price = price + (price * 0.30)
-        return 1.3
+        return config.typeOfInsurance.basic
     } else {
-        return 1.5
+        return config.typeOfInsurance.complete
     }
 }
